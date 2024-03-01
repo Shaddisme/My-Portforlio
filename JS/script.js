92de8e76d344ef2c2df67f01bc4fda8ae7c795f8
Shaddime <shadrackmwangi6@gmail.com>
2024-03-01 16:52:58 -0500

    added files for new version

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $(window).on('scroll', function(){
+$(document).ready(function() {
+    $(window).on('scroll', function() {
         var scroll = $(window).scrollTop();
-        if(scroll >= 50){
+        if (scroll >= 50) {
             $(".sticky").addClass("stickyadd");
         } else {
             $(".sticky").removeClass("stickyadd");
@@ -18,18 +18,50 @@ $(document).ready(function(){
         startDelay: 1000
     });
 
-    // Progress Bars //
+    // Progress Bars
     var waypoint = new Waypoint({
         element: document.getElementById('exp-id'),
         handler: function() {
-            var p = document.querySelectorAll('.progress-bar');
-            p[0].setAttribute("style", "width:100%;transition:1s All");
-            p[1].setAttribute("style", "width:95%;transition:1.5s All");
-            p[2].setAttribute("style", "width:85%;transition:1.7s All");
-            p[3].setAttribute("style", "width:80%;transition:2s All");
+            $('.progress-bar').eq(0).css("width", "100%").css("transition", "1s All");
+            $('.progress-bar').eq(1).css("width", "95%").css("transition", "1.5s All");
+            $('.progress-bar').eq(2).css("width", "85%").css("transition", "1.7s All");
+            $('.progress-bar').eq(3).css("width", "80%").css("transition", "2s All");
         },
-        offset:'90%'
-      });
+        offset: '90%'
+    });
 
-    
+    // Owl carousel
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        items: 1
+    });
+
+    // Contact Form Submission
+    $('#contactForm').submit(function(event) {
+        event.preventDefault();
+        var formData = {
+            'name': $('#name').val(),
+            'email': $('#email').val(),
+            'message': $('#message').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: 'https://api.web3forms.com/submit', // Replace with your API endpoint
+            headers: {
+                'Authorization': 'e71729db-1eb6-490f-b102-95e4cd342f59' // Replace with your API key
+            },
+            data: JSON.stringify(formData),
+            success: function(response) {
+                console.log(response);
+                alert('Message sent successfully!');
+                // You can redirect the user to a thank you page or show a success message here
+            },
+            error: function(error) {
+                console.error('Error:', error);
+                alert('An error occurred. Please try again later.');
+            }
+        });
+    });
 });
